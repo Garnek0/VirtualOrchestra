fedf8a6e5cce903c699f1baca7b5b091d5f28bae
Popa Vlad <vladpopa1109@gmail.com>
2024-12-22 20:25:03 +0200

    Added License

--- src/main.c
+++ src/main.c
@@ -1,10 +1,28 @@
-#include <stdio.h>
-#include <SDL2/SDL.h>
+/*	
+ *	SPDX-License-Identifier: GPL-3.0-only
+ *
+ *  Virtual Orchestra - Musical Instrument Simulation
+ *  Copyright (C) 2024 Garnek0 (Popa Vlad) and Contributors
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
+#include <stdio.h>
 #include <vo/ver.h>
 
 int main() {
-	printf("Virtual Orchestra %d.%d.%d\n", VO_VER_MAJOR, VO_VER_MINOR, VO_VER_PATCH);
+	printf("Virtual Orchestra %d.%d.%d by Garnek0 (Popa Vlad)\n", VO_VER_MAJOR, VO_VER_MINOR, VO_VER_PATCH);
 
 	return 0;
 }
